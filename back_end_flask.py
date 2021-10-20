3c7efb2c250afbc51a4fba4cf71199750d6c535f
debicosta <60646008+debicosta@users.noreply.github.com>
2021-10-20 22:29:17 +0200

    Update back_end_flask.py

--- back_end_flask.py
+++ back_end_flask.py
@@ -3,6 +3,12 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/get_sum', methods=['GET']) 
-def get_sum(x,y):
-    result = x+y
-    return jsonify(result)
+def get_sum():
+    x = request.args.get('x')
+    y = request.args.get('y')
+    result = int(x) + int(y)
+                     
+    return '{}'.format(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port = 5000, debug = True)
